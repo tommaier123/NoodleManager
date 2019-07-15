358a4f6281328a6a1dcd5b4e69098f70e146eed0
Nova_Max <40432491+tommaier123@users.noreply.github.com>
2019-07-16 00:02:09 +0900

    V1.2.2 Downlaod/delete and play button added instead of rightclick dialog

--- NoodleManager/SongControl.designer.cs
+++ NoodleManager/SongControl.designer.cs
@@ -29,17 +29,21 @@
         private void InitializeComponent()
         {
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.DownloadButton = new System.Windows.Forms.PictureBox();
+            this.PlayButton = new System.Windows.Forms.PictureBox();
+            this.Master = new NoodleManager.LabelNM();
+            this.Expert = new NoodleManager.LabelNM();
+            this.Hard = new NoodleManager.LabelNM();
+            this.Normal = new NoodleManager.LabelNM();
+            this.Easy = new NoodleManager.LabelNM();
+            this.duration = new NoodleManager.LabelNM();
+            this.artist = new NoodleManager.LabelNM();
             this.pictureBoxNM2 = new NoodleManager.PictureBoxNM();
             this.mapperName = new NoodleManager.LabelNM();
             this.songName = new NoodleManager.LabelNM();
             this.coverImage = new NoodleManager.PictureBoxNM();
-            this.artist = new NoodleManager.LabelNM();
-            this.duration = new NoodleManager.LabelNM();
-            this.Easy = new NoodleManager.LabelNM();
-            this.Normal = new NoodleManager.LabelNM();
-            this.Hard = new NoodleManager.LabelNM();
-            this.Expert = new NoodleManager.LabelNM();
-            this.Master = new NoodleManager.LabelNM();
+            ((System.ComponentModel.ISupportInitialize)(this.DownloadButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayButton)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNM2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.coverImage)).BeginInit();
             this.SuspendLayout();
@@ -49,9 +53,115 @@
             this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.progressBar1.Location = new System.Drawing.Point(645, 115);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.Size = new System.Drawing.Size(100, 24);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
             this.progressBar1.TabIndex = 12;
             this.progressBar1.Visible = false;
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.DownloadButton.BackColor = System.Drawing.Color.Lime;
+            this.DownloadButton.ErrorImage = null;
+            this.DownloadButton.InitialImage = null;
+            this.DownloadButton.Location = new System.Drawing.Point(645, 85);
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(100, 24);
+            this.DownloadButton.TabIndex = 22;
+            this.DownloadButton.TabStop = false;
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.PlayButton.BackColor = System.Drawing.Color.White;
+            this.PlayButton.ErrorImage = null;
+            this.PlayButton.InitialImage = null;
+            this.PlayButton.Location = new System.Drawing.Point(645, 55);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(100, 24);
+            this.PlayButton.TabIndex = 23;
+            this.PlayButton.TabStop = false;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // Master
+            // 
+            this.Master.AutoSize = true;
+            this.Master.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Master.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Master.Location = new System.Drawing.Point(544, 115);
+            this.Master.Name = "Master";
+            this.Master.Size = new System.Drawing.Size(71, 16);
+            this.Master.TabIndex = 21;
+            this.Master.Text = "MASTER";
+            // 
+            // Expert
+            // 
+            this.Expert.AutoSize = true;
+            this.Expert.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Expert.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Expert.Location = new System.Drawing.Point(470, 115);
+            this.Expert.Name = "Expert";
+            this.Expert.Size = new System.Drawing.Size(68, 16);
+            this.Expert.TabIndex = 20;
+            this.Expert.Text = "EXPERT";
+            // 
+            // Hard
+            // 
+            this.Hard.AutoSize = true;
+            this.Hard.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Hard.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Hard.Location = new System.Drawing.Point(413, 115);
+            this.Hard.Name = "Hard";
+            this.Hard.Size = new System.Drawing.Size(51, 16);
+            this.Hard.TabIndex = 19;
+            this.Hard.Text = "HARD";
+            // 
+            // Normal
+            // 
+            this.Normal.AutoSize = true;
+            this.Normal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Normal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Normal.Location = new System.Drawing.Point(336, 115);
+            this.Normal.Name = "Normal";
+            this.Normal.Size = new System.Drawing.Size(71, 16);
+            this.Normal.TabIndex = 18;
+            this.Normal.Text = "NORMAL";
+            // 
+            // Easy
+            // 
+            this.Easy.AutoSize = true;
+            this.Easy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Easy.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Easy.Location = new System.Drawing.Point(282, 115);
+            this.Easy.Name = "Easy";
+            this.Easy.Size = new System.Drawing.Size(48, 16);
+            this.Easy.TabIndex = 17;
+            this.Easy.Text = "EASY";
+            // 
+            // duration
+            // 
+            this.duration.AutoSize = true;
+            this.duration.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.duration.ForeColor = System.Drawing.Color.White;
+            this.duration.Location = new System.Drawing.Point(172, 105);
+            this.duration.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.duration.Name = "duration";
+            this.duration.Size = new System.Drawing.Size(92, 33);
+            this.duration.TabIndex = 16;
+            this.duration.Text = "10:30";
+            // 
+            // artist
+            // 
+            this.artist.AutoSize = true;
+            this.artist.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.artist.ForeColor = System.Drawing.Color.White;
+            this.artist.Location = new System.Drawing.Point(174, 43);
+            this.artist.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.artist.Name = "artist";
+            this.artist.Size = new System.Drawing.Size(50, 24);
+            this.artist.TabIndex = 15;
+            this.artist.Text = "Artist";
             // 
             // pictureBoxNM2
             // 
@@ -101,90 +211,13 @@
             this.coverImage.TabIndex = 1;
             this.coverImage.TabStop = false;
             // 
-            // artist
-            // 
-            this.artist.AutoSize = true;
-            this.artist.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.artist.ForeColor = System.Drawing.Color.White;
-            this.artist.Location = new System.Drawing.Point(174, 43);
-            this.artist.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.artist.Name = "artist";
-            this.artist.Size = new System.Drawing.Size(50, 24);
-            this.artist.TabIndex = 15;
-            this.artist.Text = "Artist";
-            // 
-            // duration
-            // 
-            this.duration.AutoSize = true;
-            this.duration.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.duration.ForeColor = System.Drawing.Color.White;
-            this.duration.Location = new System.Drawing.Point(172, 105);
-            this.duration.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.duration.Name = "duration";
-            this.duration.Size = new System.Drawing.Size(92, 33);
-            this.duration.TabIndex = 16;
-            this.duration.Text = "10:30";
-            // 
-            // Easy
-            // 
-            this.Easy.AutoSize = true;
-            this.Easy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Easy.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.Easy.Location = new System.Drawing.Point(282, 115);
-            this.Easy.Name = "Easy";
-            this.Easy.Size = new System.Drawing.Size(48, 16);
-            this.Easy.TabIndex = 17;
-            this.Easy.Text = "EASY";
-            // 
-            // Normal
-            // 
-            this.Normal.AutoSize = true;
-            this.Normal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Normal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.Normal.Location = new System.Drawing.Point(336, 115);
-            this.Normal.Name = "Normal";
-            this.Normal.Size = new System.Drawing.Size(71, 16);
-            this.Normal.TabIndex = 18;
-            this.Normal.Text = "NORMAL";
-            // 
-            // Hard
-            // 
-            this.Hard.AutoSize = true;
-            this.Hard.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Hard.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.Hard.Location = new System.Drawing.Point(413, 115);
-            this.Hard.Name = "Hard";
-            this.Hard.Size = new System.Drawing.Size(51, 16);
-            this.Hard.TabIndex = 19;
-            this.Hard.Text = "HARD";
-            // 
-            // Expert
-            // 
-            this.Expert.AutoSize = true;
-            this.Expert.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Expert.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.Expert.Location = new System.Drawing.Point(470, 115);
-            this.Expert.Name = "Expert";
-            this.Expert.Size = new System.Drawing.Size(68, 16);
-            this.Expert.TabIndex = 20;
-            this.Expert.Text = "EXPERT";
-            // 
-            // Master
-            // 
-            this.Master.AutoSize = true;
-            this.Master.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Master.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.Master.Location = new System.Drawing.Point(544, 115);
-            this.Master.Name = "Master";
-            this.Master.Size = new System.Drawing.Size(71, 16);
-            this.Master.TabIndex = 21;
-            this.Master.Text = "MASTER";
-            // 
             // SongControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(27)))), ((int)(((byte)(31)))));
+            this.Controls.Add(this.PlayButton);
+            this.Controls.Add(this.DownloadButton);
             this.Controls.Add(this.Master);
             this.Controls.Add(this.Expert);
             this.Controls.Add(this.Hard);
@@ -199,6 +232,8 @@
             this.Controls.Add(this.coverImage);
             this.Name = "SongControl";
             this.Size = new System.Drawing.Size(780, 160);
+            ((System.ComponentModel.ISupportInitialize)(this.DownloadButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayButton)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNM2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.coverImage)).EndInit();
             this.ResumeLayout(false);
@@ -220,5 +255,7 @@
         public LabelNM Hard;
         public LabelNM Expert;
         public LabelNM Master;
+        private System.Windows.Forms.PictureBox DownloadButton;
+        private System.Windows.Forms.PictureBox PlayButton;
     }
 }
